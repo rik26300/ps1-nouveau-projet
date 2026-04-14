1ce36178e91afeeb37e9ec28ae0cea867782853b
rik26300 <104155733+rik26300@users.noreply.github.com>
2026-04-14 07:28:29 +0200

    Augmente la version du script / Bump script version

--- prepare-nouveau-projet.ps1
+++ prepare-nouveau-projet.ps1
@@ -10,7 +10,7 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-$ScriptVersion = [Version] '2.0.0'
+$ScriptVersion = [Version] '2.0.1'
 $CurrentConfigVersion = 13
 $LegacyConfigVersion = 1
 $ConfigFileName = 'prepare-nouveau-projet.config.json'
